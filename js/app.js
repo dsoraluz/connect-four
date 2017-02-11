03a61a644785b778758947b38fc46c13f5c78e6c
dsoraluz <d.soraluz@gmail.com>
2017-02-10 19:04:35 -0500

    moves and winner determination coded

--- js/app.js
+++ js/app.js
@@ -4,14 +4,81 @@ var myGlobalConnectGame;
 
 
 $(document).ready(function(){
-  myGlobalConnectGame = new ConnectFour('Daniel', 'Nizar');
+  var player1 = prompt('Enter name for player 1.');
+  var player2 = prompt('Enter name for player 2.');
+
+  myGlobalConnectGame = new ConnectFour(player1, player2);
 
 
   console.log(myGlobalConnectGame);
 
   renderBoard();
 
+  //Made a funciton be
+  updateCurrentPlayer();
+
+  $('#drop0').click(function(){
+    myGlobalConnectGame.playChecker(0);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop1').click(function(){
+    myGlobalConnectGame.playChecker(1);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop2').click(function(){
+    myGlobalConnectGame.playChecker(2);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop3').click(function(){
+    myGlobalConnectGame.playChecker(3);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop4').click(function(){
+    myGlobalConnectGame.playChecker(4);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop5').click(function(){
+    myGlobalConnectGame.playChecker(5);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+  $('#drop6').click(function(){
+    myGlobalConnectGame.playChecker(6);
+    renderBoard();
+    updateCurrentPlayer();
+  });
+
 });
+
+function updateCurrentPlayer(){
+  $('#current-player').css("visibility", "visible");
+
+  if(myGlobalConnectGame.currentPlayer === myGlobalConnectGame.player1){
+    $('#current-player').html(myGlobalConnectGame.player1+"'s Turn");
+    $('#current-player').addClass('player-1');
+    $('#current-player').removeClass('player-2');
+
+
+    $('#drop-buttons').addClass('turn-player-1');
+    $('#drop-buttons').removeClass('turn-player-2');
+  }
+  else {
+    $('#current-player').html(myGlobalConnectGame.player2 +"'s Turn");
+    $('#current-player').addClass('player-2');
+    $('#current-player').removeClass('player-1');
+
+
+    $('#drop-buttons').addClass('turn-player-2');
+    $('#drop-buttons').removeClass('turn-player-1');
+  }
+  $('#current-player').css('visibility', 'visible');
+  $('#drop-buttons').css('visibility', 'visible');
+}
 
 
 function renderBoard(){
@@ -31,6 +98,7 @@ function renderBoard(){
         checkerClass = 'checker-player-2';
       }
       else{
+        //empty slot
         checkerClass='';
       }
 
--- js/connect-four.js
+++ js/connect-four.js
@@ -1,7 +1,7 @@
 
 //Constructor for connect-four game.
 function ConnectFour(player1Name, player2Name){
-  //6 x 7 array
+  //6 x 7 array to represent checkers grid.
 this.board = [
   // 0       1     2    3     4     5     6
     [null, null, null, null, null, null, null], //0
@@ -26,3 +26,162 @@ this.board = [
   }
 
 }
+
+//Function that will check available slots for players to play
+//For checkers, we want to check from the bottom up.
+ConnectFour.prototype.playChecker= function (columnNumber) {
+
+  //To check
+  if (this.winner !== null){
+    return false;
+  }
+  var playerPositionFound = "not found";
+
+  //Checks bottom most row and moves up vertically.
+  for (var i = (this.board.length - 1); i > 0 ; i -= 1)
+  {
+    //As soon as we find an empty position, break.
+    if (this.board[i][columnNumber] === null){
+      playerPositionFound = i;
+      break;
+    }
+  }
+
+  //if never found position (still equals not found) => column is full
+  //INVALID MOVE
+
+  if(playerPositionFound === 'not found'){
+    return false;
+  }
+
+
+  if (this.currentPlayer === this.player1){
+    //If Active player is 1, fill position with a 1;
+    this.board[playerPositionFound][columnNumber] = 1;
+    //switch turn
+    this.currentPlayer = this.player2;
+  }
+  else{
+    //If active player is 2, fill position 2.
+    this.board[playerPositionFound][columnNumber] = 2;
+    //switch turns
+    this.currentPlayer = this.player1;
+  }
+
+  this._checkWinner();
+
+  console.log("Winner", this.winner);
+
+  return true;
+
+};
+
+
+ConnectFour.prototype._checkWinner = function () {
+  var row, col, current;
+
+  // Horizontal
+  //   rows: all
+  //   cols: 0-3
+  for (row = 0; row < this.board.length; row += 1) {
+    for (col = 0; col <= 3; col += 1) {
+      current = this.board[row][col];
+
+      if (current !== null &&
+          current === this.board[row][col + 1] &&
+          current === this.board[row][col + 2] &&
+          current === this.board[row][col + 3]) {
+        if (current === 1) {
+          this.winner = this.player1;
+        } else {
+          this.winner = this.player2;
+        }
+        break;
+      }
+    }
+
+    if (this.winner !== null) {
+      break;
+    }
+  }
+
+
+  // Vertical
+  //   rows: 0-2
+  //   cols: all
+  for (row = 0; row <= 2; row += 1) {
+    var currentRow = this.board[row];
+
+    for (col = 0; col < currentRow.length; col += 1) {
+      current = this.board[row][col];
+
+      if (current !== null &&
+          current === this.board[row + 1][col] &&
+          current === this.board[row + 2][col] &&
+          current === this.board[row + 3][col]) {
+        if (current === 1) {
+          this.winner = this.player1;
+        } else {
+          this.winner = this.player2;
+        }
+        break;
+      }
+    }
+
+    if (this.winner !== null) {
+      break;
+    }
+  }
+
+
+  // Down-Right / Up-Left
+  //   rows: 0-2
+  //   cols: 0-3
+  for (row = 0; row <= 2; row += 1) {
+    for (col = 0; col <= 3; col += 1) {
+      current = this.board[row][col];
+
+      if (current !== null &&
+          current === this.board[row + 1][col + 1] &&
+          current === this.board[row + 2][col + 2] &&
+          current === this.board[row + 3][col + 3]) {
+        if (current === 1) {
+          this.winner = this.player1;
+        } else {
+          this.winner = this.player2;
+        }
+        break;
+      }
+    }
+
+    if (this.winner !== null) {
+      break;
+    }
+  }
+
+
+  // Down-Left / Up-Right
+  //   rows: 0-2
+  //   cols: 3-6
+  for (row = 0; row <= 2; row += 1) {
+    for (col = 3; col <= 6; col += 1) {
+      current = this.board[row][col];
+
+      if (current !== null &&
+          current === this.board[row + 1][col - 1] &&
+          current === this.board[row + 2][col - 2] &&
+          current === this.board[row + 3][col - 3]) {
+        if (current === 1) {
+          this.winner = this.player1;
+        } else {
+          this.winner = this.player2;
+        }
+        break;
+      }
+    }
+
+    if (this.winner !== null) {
+      break;
+    }
+  }
+};
